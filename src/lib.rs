9f8e00c2655c67bf260e416b5be8a52e70a62ad5
Anton Sapunov <fort.sav.28@gmail.com>
2020-03-29 22:00:59 +0700

    Added some cache to avoid permanently download Added html-parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
-use std::fs;
+use scraper::Html;
+use scraper::Selector;
+use std::fs::File;
 use std::io::Read;
+use std::path::Path;
 
 pub struct Page {
     url: String,
@@ -14,17 +17,39 @@ impl Page {
     pub fn new(url: &str) -> Page {
         let url = url.to_owned();
         let data = Self::download_page(&url);
+        let document = Html::parse_document(&data);
+        Self::test_output(document);
         let posts = Self::get_all_posts(&data);
         Page { url, data, posts }
     }
 
     fn download_page(url: &str) -> String {
+        if Path::new("retelling.txt").exists() {
+            let mut file = File::open("retelling.txt").unwrap();
+            let mut buffer = String::new();
+
+            file.read_to_string(&mut buffer)
+                .expect("Cannot read from file!");
+            return buffer;
+        }
+
+        // If there are no needed file let`s download it
         let mut res = reqwest::blocking::get(url).expect("Cannot perform get reqwest.");
         let mut body = String::new();
         res.read_to_string(&mut body)
             .expect("Problem while read response to string");
 
         body
+    }
+
+    fn test_output(parsed_html: scraper::Html) {
+        let post_selector =
+            Selector::parse(r#"div[class="pi_text"]"#).expect("Error while parse selector!");
+        let first_post = parsed_html
+            .select(&post_selector)
+            .next()
+            .expect("Error while getting the first post!");
+        dbg!(first_post);
     }
 
     fn get_all_posts(page: &str) -> Vec<Post> {
