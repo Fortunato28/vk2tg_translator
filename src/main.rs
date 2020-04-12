d7c9a6cf688cf197d2386401a0bdbca7527b8917
Anton Sapunov <fort.sav.28@gmail.com>
2020-04-12 16:31:53 +0700

    Part of cli realized

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,25 @@ use clap::{Arg, App};
 
 #[tokio::main]
 async fn main() {
-    //let cli = App::new("Translator from vkontakte group to telegram channel")
-    //    .version("0.1.0")
-    //    .author("Sapunov Anton <fort.sav.28@gmail.com")
-    //    .args_from_usage(
-    //        "
-    //        -f, --from=[link] 'Link to some group in vk'
-    //        -t, --to 'Telegram channel name'
-    //        -s, --storage 'Filename for storing already published post'
-    //        "
-    //    )
-    //    .get_matches();
+    let cli = App::new("Translator from vkontakte group to telegram channel")
+        .version("0.1.0")
+        .author("Sapunov Anton <fort.sav.28@gmail.com")
+        .args_from_usage(
+            "
+            -f, --from=[link] 'Link to some group in vk'
+            -t, --to=[channel name] 'Telegram channel name'
+            -s, --storage[storage filename] 'Filename for storing already published post'
+            "
+        )
+        .get_matches();
 
-    //let source_vk_group = cli.value_of("from").expect("No required [from] parameter");
-    //dbg!(&source_vk_group);
+    let source_vk_group = cli.value_of("from").expect("No required [from] parameter");
+    let target_channel = cli.value_of("to").expect("No required [to] parameter");
+    let storage = cli.value_of("storage").unwrap_or("test_url_storage.txt");
+
+    dbg!(&source_vk_group);
+    dbg!(&target_channel);
+    dbg!(&storage);
 
     run().await;
 }
