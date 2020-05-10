a7a23894c95c4e8feb525d379c0efc120bc9a41b
Anton Sapunov <fort.sav.28@gmail.com>
2020-05-10 17:18:06 +0700

    Everything ready to send prepare_request

--- src/group_data.rs
+++ src/group_data.rs
@@ -1,16 +1,25 @@
 use anyhow::{anyhow, Result};
+use std::env;
 
 #[derive(Debug)]
 pub struct Group {
     pub link: String,
     pub id: String,
+    pub access_token: String,
 }
 
 impl Group {
     pub fn new(link: &str) -> Group {
         let link = link.to_owned();
         let id = Self::extract_group_id(&link).expect("Probably your group link is wrong.");
-        Group { link, id }
+
+        let access_token =
+            env::var("VK_GROUP_TOKEN").expect("No VK_GROUP_TOKEN environment variable");
+        Group {
+            link,
+            id,
+            access_token,
+        }
     }
 
     fn extract_group_id(link: &str) -> Result<String> {
--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,9 @@ use teloxide::types;
 mod group_data;
 
 pub async fn run(source: &str, target_channel: &str, storage: &str) {
-    let new_posts = vec!["str1".to_owned(), "str2".to_owned()];
+    //let new_posts = vec!["str1".to_owned(), "str2".to_owned()];
     let group = group_data::Group::new(source);
-    let meta_data = get_meta_data(&group.id, "").await;
+    let meta_data = get_meta_data(&group).await;
 
     //let bot = Bot::from_env();
 
@@ -27,14 +27,16 @@ struct MetaData {
     ts: u64,
 }
 
-async fn get_meta_data(group_id: &str, access_token: &str) -> Result<MetaData> {
-    let group_id = "192827874";
-    let acces_token = "";
+async fn get_meta_data(group: &group_data::Group) -> Result<MetaData> {
     let request = r#"https://api.vk.com/method/groups.getLongPollServer/?&v=5.103&group_id="#;
     //\&access_token\=
 
-    let result_request = format!("{}{}&access_token={}", request, group_id, access_token);
+    let result_request = format!(
+        "{}{}&access_token={}",
+        request, group.id, group.access_token
+    );
     dbg!(&result_request);
+
     let key = "key".to_owned();
     let server = "server".to_owned();
     let ts = 0;
