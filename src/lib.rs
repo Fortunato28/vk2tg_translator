3dfac876b4a2958a476e5fad17edb9b84403ccce
Anton Sapunov <fort.sav.28@gmail.com>
2020-05-10 20:03:26 +0700

    Response serialized

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use anyhow::{anyhow, Result};
+use serde::{Deserialize, Serialize};
 use teloxide::prelude::*;
 use teloxide::types;
 
@@ -8,6 +9,7 @@ pub async fn run(source: &str, target_channel: &str, storage: &str) {
     //let new_posts = vec!["str1".to_owned(), "str2".to_owned()];
     let group = group_data::Group::new(source);
     let meta_data = get_meta_data(&group).await;
+    dbg!(&meta_data);
 
     //let bot = Bot::from_env();
 
@@ -20,25 +22,37 @@ pub async fn run(source: &str, target_channel: &str, storage: &str) {
     //}
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct MetaData {
     key: String,
     server: String,
-    ts: u64,
+    ts: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct VkResponse {
+    pub response: String,
 }
 
 async fn get_meta_data(group: &group_data::Group) -> Result<MetaData> {
     let request = r#"https://api.vk.com/method/groups.getLongPollServer/?&v=5.103&group_id="#;
-    //\&access_token\=
 
     let result_request = format!(
         "{}{}&access_token={}",
         request, group.id, group.access_token
     );
-    dbg!(&result_request);
 
-    let key = "key".to_owned();
-    let server = "server".to_owned();
-    let ts = 0;
-    Ok(MetaData { key, server, ts })
+    let res = reqwest::get(&result_request)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // Remove responce field from response
+    let mut res = res.replace(r#"{"response":"#, "");
+    res.pop();
+
+    let meta_data: MetaData = serde_json::from_str(&res)?;
+    Ok(meta_data)
 }
