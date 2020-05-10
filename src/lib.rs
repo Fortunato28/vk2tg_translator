63e6143b5cbb306d832422c6427c14004c2e03eb
Anton Sapunov <fort.sav.28@gmail.com>
2020-05-10 20:08:17 +0700

    Meta data getting extracted into another file

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-use anyhow::{anyhow, Result};
-use serde::{Deserialize, Serialize};
 use teloxide::prelude::*;
 use teloxide::types;
 
 mod group_data;
+mod long_polling_meta_data;
 
 pub async fn run(source: &str, target_channel: &str, storage: &str) {
     //let new_posts = vec!["str1".to_owned(), "str2".to_owned()];
     let group = group_data::Group::new(source);
-    let meta_data = get_meta_data(&group).await;
+    let meta_data = long_polling_meta_data::get_meta_data(&group).await;
     dbg!(&meta_data);
 
     //let bot = Bot::from_env();
@@ -20,39 +19,4 @@ pub async fn run(source: &str, target_channel: &str, storage: &str) {
     //        .log_on_error()
     //        .await;
     //}
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MetaData {
-    key: String,
-    server: String,
-    ts: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct VkResponse {
-    pub response: String,
-}
-
-async fn get_meta_data(group: &group_data::Group) -> Result<MetaData> {
-    let request = r#"https://api.vk.com/method/groups.getLongPollServer/?&v=5.103&group_id="#;
-
-    let result_request = format!(
-        "{}{}&access_token={}",
-        request, group.id, group.access_token
-    );
-
-    let res = reqwest::get(&result_request)
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-
-    // Remove responce field from response
-    let mut res = res.replace(r#"{"response":"#, "");
-    res.pop();
-
-    let meta_data: MetaData = serde_json::from_str(&res)?;
-    Ok(meta_data)
 }
---
+++ src/long_polling_meta_data.rs
@@ -0,0 +1,34 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+use crate::group_data;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MetaData {
+    key: String,
+    server: String,
+    ts: String,
+}
+
+pub async fn get_meta_data(group: &group_data::Group) -> Result<MetaData> {
+    let request = r#"https://api.vk.com/method/groups.getLongPollServer/?&v=5.103&group_id="#;
+
+    let result_request = format!(
+        "{}{}&access_token={}",
+        request, group.id, group.access_token
+    );
+
+    let res = reqwest::get(&result_request)
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    // Remove responce field from response
+    let mut res = res.replace(r#"{"response":"#, "");
+    res.pop();
+
+    let meta_data: MetaData = serde_json::from_str(&res)?;
+    Ok(meta_data)
+}
