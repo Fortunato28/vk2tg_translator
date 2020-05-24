21a563d533248a02b2f156cee482fda519ddee60
Anton Sapunov <fort.sav.28@gmail.com>
2020-05-25 00:57:27 +0700

    Test fixed, test_json fixed

--- src/lp_response.rs
+++ src/lp_response.rs
@@ -78,87 +78,88 @@ mod tests {
         let parsed_response = parse_response(test_response).unwrap();
         let ts = parsed_response.get_ts().unwrap();
         assert_eq!(ts, 564);
-        //match parsed_response {
-        //    Response::Ok(x) => assert_eq!(x.ts, 564),
-        //    _ => (),
-        //}
-        //assert_eq!(ts, 564);
     }
 
-    //#[test]
-    //fn bad_ts() {
-    //    let test_response = r#"{"ts":"", "updates":"nothing"}"#;
-    //    let ts_error = parse_response(test_response);
-    //    assert!(ts_error.is_err());
-    //}
+    #[test]
+    fn bad_ts() {
+        let test_response = r#"{"ts":"", "updates":[]}"#;
+        let parsed_response = parse_response(test_response);
+        assert!(parsed_response.is_err());
+    }
 
-    //#[test]
-    //fn get_update() {
-    //    let test_response = r#"{
-    //"ts":"16",
-    //"updates":
-    //[
-    //    {
-    //        "type":"wall_post_new",
-    //        "object":
-    //        {
-    //            "id":16,
-    //            "from_id":69751141,
-    //            "owner_id":-192827874,
-    //            "date":1589992602,
-    //            "marked_as_ads":0,
-    //            "post_type":"post",
-    //            "text":"fvdfvd",
-    //            "can_edit":1,
-    //            "created_by":69751141,
-    //            "can_delete":1,
-    //            "attachments":
-    //            [
-    //                {
-    //                    "type":"photo",
-    //                    "photo":
-    //                    {
-    //                        "id":457239019,
-    //                        "album_id":-8,
-    //                        "owner_id":-192827874,
-    //                        "user_id":100,
-    //                        "photo_75":"https:\/\/sun1-97.userapi.com\/-jvQYpQNCxoUuc7dsIHzKoSQUYXMml3nX6IbsA\/mpvsDR8NaPA.jpg",
-    //                        "photo_130":"https:\/\/sun1-89.userapi.com\/DwIt8XcKO5bdFH2a56dP9w7ApdLUZsxqi5kgXw\/q1dbkQ3MaBo.jpg",
-    //                        "photo_604":"https:\/\/sun1-92.userapi.com\/JMKB28PJw50fsCLfDkjq6Tijn7BI2ar3tz2xgw\/AQiQEFRT5ps.jpg",
-    //                        "photo_807":"https:\/\/sun1-20.userapi.com\/hZpdOxh1eH3bWQsGluhGdjBH_aVs8fAB2vH-aA\/mKLKpumhFVQ.jpg",
-    //                        "width":700,
-    //                        "height":488,
-    //                        "text":"",
-    //                        "date":1589992602,
-    //                        "post_id":16,
-    //                        "access_key":"71882b9b66a35769aa"
-    //                    }
-    //                }
-    //            ],
-    //            "comments":
-    //            {
-    //                "count":0
-    //            }
-    //        },
-    //        "group_id":192827874,
-    //        "event_id":"43e633c025f3aae41d90f1ef5d2cbb4aaaf719b0"
-    //    }
-    //]
-    //}"#;
-    //    let (_, _) = parse_response(test_response).unwrap();
-    //    assert_eq!(3, 3);
-    //}
+    #[test]
+    fn get_update() {
+        let test_response = r#"{
+    "ts":"16",
+    "updates":
+    [
+        {
+            "type":"wall_post_new",
+            "object":
+            {
+                "id":16,
+                "from_id":69751141,
+                "owner_id":-192827874,
+                "date":1589992602,
+                "marked_as_ads":0,
+                "post_type":"post",
+                "text":"fvdfvd",
+                "can_edit":1,
+                "created_by":69751141,
+                "can_delete":1,
+                "attachments":
+                [
+                    {
+                        "type":"photo",
+                        "photo":
+                        {
+                            "id":457239019,
+                            "album_id":-8,
+                            "owner_id":-192827874,
+                            "user_id":100,
+                            "photo_75":"https:\/\/sun1-97.userapi.com\/-jvQYpQNCxoUuc7dsIHzKoSQUYXMml3nX6IbsA\/mpvsDR8NaPA.jpg",
+                            "photo_130":"https:\/\/sun1-89.userapi.com\/DwIt8XcKO5bdFH2a56dP9w7ApdLUZsxqi5kgXw\/q1dbkQ3MaBo.jpg",
+                            "photo_604":"https:\/\/sun1-92.userapi.com\/JMKB28PJw50fsCLfDkjq6Tijn7BI2ar3tz2xgw\/AQiQEFRT5ps.jpg",
+                            "photo_807":"https:\/\/sun1-20.userapi.com\/hZpdOxh1eH3bWQsGluhGdjBH_aVs8fAB2vH-aA\/mKLKpumhFVQ.jpg",
+                            "width":700,
+                            "height":488,
+                            "text":"",
+                            "date":1589992602,
+                            "post_id":16,
+                            "access_key":"71882b9b66a35769aa"
+                        }
+                    }
+                ],
+                "comments":
+                {
+                    "count":0
+                }
+            },
+            "group_id":192827874,
+            "event_id":"43e633c025f3aae41d90f1ef5d2cbb4aaaf719b0"
+        }
+    ]
+    }"#;
+        let parsed_response = parse_response(test_response).unwrap();
+        match parsed_response {
+            Response::Ok(resp) => assert!(!resp.posts.is_empty()),
+            Response::Err(_) => panic!("Wrong response parsing"),
+        }
+    }
 
-    //#[test]
-    //fn empty_updates() {
-    //    let test_response = r#"{
-    //"ts":"16",
-    //"updates":
-    //[]
-    //}"#;
-    //    let (_, updates) = parse_response(test_response).unwrap();
-    //    assert!(updates.is_empty());
-    //}
+    #[test]
+    fn empty_updates() {
+        let test_response = r#"{
+    "ts":"16",
+    "updates":
+    []
+    }"#;
+        let parsed_response = parse_response(test_response).unwrap();
+        match parsed_response {
+            Response::Ok(resp) => assert!(resp.posts.is_empty()),
+            Response::Err(_) => panic!("Wrong response parsing"),
+        }
+    }
 
     //#[test]
     //fn bad_data() {
